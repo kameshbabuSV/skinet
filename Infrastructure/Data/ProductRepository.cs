84460461b71b78964430b1bd3aeb4c93fd71e59e
kameshbabuSV <svkameshbabu@gmail.com>
2022-05-17 00:46:17 +0530

    section 4

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -22,7 +22,7 @@ namespace Infrastructure.Data
             return await _context.ProductBrands.ToListAsync();
         }
 
-        public async Task<Product> GetProductByIdAsync(int id)
+        public async Task<Product?> GetProductByIdAsync(int id)
         {
             return await _context.Products
             .Include(p =>p.ProductBrand)
